c30582439d2e5ad23251447a3d2689e94777f9ce
Benmuiruri <ben.muiruri13@gmail.com>
2022-08-08 20:18:49 +0300

    Generate static controller Generate new React component Add route to show React component

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  
+  get 'greetings', to: 'greetings#index'
+
+    #Forward all requests to StaticController#index except root path
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  #Forward root to StaticController#index
+  root 'static#index'
 end
